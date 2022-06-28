34e8c2a7464b1b1ff2d0c92344a85b9ef8d0cea3
Matt <matthew.t.kissinger@gmail.com>
2022-06-28 13:02:55 -0700

    added in a start timer function

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import Confetti from 'react-confetti'
 
 export default function App() {
 
+    // 1. Put Real Dots on the Dice
+    // 2. Track the Number of Rolls -- DONE
+    // 3. Track the time it took to win
+    // 4. Save Number of Rolls and the Time it Took to win to LocalStorage
+    // 5. Display get Data from LocalStorage and Display it for the User
+
     const [dice, setDice] = React.useState(allNewDice());
 
     const [tenzies, setTenzies] = React.useState(false);
 
+    const [rolls, setRolls] = React.useState(0);
+
+    const [timer, setTimer] = React.useState('00:00');
+
+    // check for game winning conditions
     React.useEffect(() => {
 
         // Bob's solution
@@ -29,6 +40,13 @@ export default function App() {
         }
     }, [dice]);
 
+    // check if game is won to stop the timer 
+    React.useEffect(() => {
+        if (tenzies) {
+            // stop timer function 
+        }
+    }, [tenzies])
+
     const dieElements = dice.map((die) => {
         return (
             <Die 
@@ -39,6 +57,29 @@ export default function App() {
             />
         )
     })
+
+    let seconds = 0;
+    let interval = null;
+
+    function updateTimer(){
+        seconds++;
+
+        let secs = seconds % 60;
+        let mins = Math.floor(seconds / 60);
+
+        if (secs < 10) secs = `0${secs}`;
+        if (mins < 10) mins = `0${mins}`;
+
+        console.log(`${mins}:${secs}`)
+
+        setTimer(`${mins}:${secs}`);
+    }
+
+    function startTimer() {
+        if (!interval) {
+            interval = setInterval(updateTimer, 1000);
+        }
+    }
 
     function allNewDice() {
         const newDice = [];
@@ -65,6 +106,10 @@ export default function App() {
                     {...die, value: Math.floor(Math.random() * 6) + 1}
                 })
             })
+            startTimer();
+            setRolls(oldRolls => {
+                return oldRolls + 1
+            })
         } else {
             newGame();
         }
@@ -84,6 +129,7 @@ export default function App() {
     function newGame() {
         setTenzies(false);
         setDice(allNewDice);
+        setRolls(0);
     }
 
     return (
@@ -91,6 +137,10 @@ export default function App() {
             {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="game-stats-container">
+                <div className="timer">Time: {timer} </div>
+                <h3 className="roll-counter"># of Rolls: {rolls}</h3>
+            </div>
             <div className="dice-container">
                 {dieElements}
             </div>
